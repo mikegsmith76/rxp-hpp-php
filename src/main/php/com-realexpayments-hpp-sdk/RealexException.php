1454ce6a3062d0db499a496b98bef70366f047c0
Mike Smith <mail@mikegsmith.co.uk>
2025-01-07 08:50:03 +0000

    Bump symfony/validator to supported version

--- src/main/php/com-realexpayments-hpp-sdk/RealexException.php
+++ src/main/php/com-realexpayments-hpp-sdk/RealexException.php
@@ -1,11 +1,9 @@
 <?php
 
-
 namespace com\realexpayments\hpp\sdk;
 
 use Exception;
 use RuntimeException;
-
 
 /**
  * An exception class for general Realex SDK errors. All other SDK exceptions will extend this class.
@@ -15,9 +13,7 @@ use RuntimeException;
  */
 class RealexException extends RuntimeException
 {
-
     const serialVersionUID = -2270549234447218179;
-
 
     /**
      * RealexException constructor.
@@ -25,11 +21,8 @@ class RealexException extends RuntimeException
      * @param string $message
      * @param Exception $e
      */
-    public function __construct($message, Exception $e = null)
+    public function __construct(string $message, ?Exception $e = null)
     {
         parent::__construct($message, 0, $e);
-
     }
-
-
 }
